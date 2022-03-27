c689d4708f56fa5a88dab84f812b9d77115047c7
EHinrich <90458838+EHinrich@users.noreply.github.com>
2022-03-27 14:31:07 +0300

    Update form.php

--- form.php
+++ form.php
@@ -31,17 +31,29 @@
                          type="email" />
               </label><br />
       
-              <!--<label>-->
+              <label>
                   Год рождения:<br />
-                  <select name="year"></select>
-                   <script>$(document).ready(function() {
-                            $("#date").datepicker({
-                                changeYear:true,
-                                yearRange: "2005:2015"
-                            });
-                        });
-                       </script>
-              <!--</label>--><br />
+                  <select name="year">
+                      <option value="1900">1900</option>
+                      <option value="1901">1901</option>
+                      <option value="1902">1902</option>
+                      <option value="1903">1903</option>
+                      <option value="1904">1904</option>
+                      <option value="1905">1905</option>
+                      <option value="1906">1906</option>
+                      <option value="1907">1907</option>
+                      <option value="1908">1908</option>
+                      <option value="1909">1909</option>
+                      <option value="1910">1910</option>
+                      <option value="1911">1911</option>
+                      <option value="1912">1912</option>
+                      <option value="1913">1913</option>
+                      <option value="1914">1914</option>
+                      <option value="1915">1915</option>
+                      <option value="1916">1916</option>
+                      <option value="1917">1917</option>
+                   </select>
+              </label><br />
       
               Пол:<br />
               <label>
